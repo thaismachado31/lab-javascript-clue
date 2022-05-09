c4ce12c9cb3fa1592c096ccb8fa544eb1ebd6b0b
thaismachado31 <thaismachado31@gmail.com>
2022-05-09 15:49:14 -0300

    lab finalizado

--- src/clue.js
+++ src/clue.js
@@ -2,17 +2,18 @@
 
 // Suspects Array
 
-const suspectsArray = [ 
-  { 
+const suspectsArray = [
+  {
     firstName: 'Jacob',
     lastName: 'Green',
     occupation: 'Entrepreneur',
     age: 45,
     description: 'He has a lot of connections',
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: 'green' 
+    image:
+      'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
   },
-  { 
+  {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
@@ -27,7 +28,8 @@ const suspectsArray = [
     occupation: 'Designer',
     age: 22,
     description: 'Billionaire video game designer',
-    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    image:
+      'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
     color: 'purple'
   },
   {
@@ -44,7 +46,8 @@ const suspectsArray = [
     lastName: 'Peacock',
     occupation: 'Socialité',
     age: 36,
-    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    description:
+      'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
     color: 'blue'
   },
@@ -53,10 +56,12 @@ const suspectsArray = [
     lastName: 'Mustard',
     occupation: 'Retired Football player',
     age: 62,
-    description: 'He is a former football player who tries to get by on his former glory',
-    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    description:
+      'He is a former football player who tries to get by on his former glory',
+    image:
+      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
     color: 'yellow'
-  },
+  }
 ];
 
 // Rooms Array
@@ -65,26 +70,22 @@ const roomsArray = [
   {
     name: 'Dining Room'
   },
-  {name: 'Conservatory'
-  },
+  { name: 'Conservatory' },
   {
     name: 'Kitchen'
   },
   {
     name: 'Study'
   },
-  {name: 'Library'
-  },
+  { name: 'Library' },
   {
     name: 'Billiard Room'
   },
   {
     name: 'Lounge'
   },
-  {name: 'Ballroom'
-  },
-  {name: 'Hall'
-  },
+  { name: 'Ballroom' },
+  { name: 'Hall' },
   {
     name: 'Spa'
   },
@@ -102,7 +103,7 @@ const roomsArray = [
   },
   {
     name: 'Patio'
-  },
+  }
 ];
 
 // Weapons Array
@@ -124,34 +125,33 @@ const weaponsArray = [
     name: 'dumbbell',
     weight: 30
   },
-{
-  name: 'poison',
-  weight: 2
-},
-{
-  name: 'axe',
-  weight: 15
-},
-{
-  name: 'bat',
-  weight: 13
-},
-{
-  name: 'trophy',
-  weight: 25
-},
-{
-  name: 'pistol',
-  weight: 20
-},
+  {
+    name: 'poison',
+    weight: 2
+  },
+  {
+    name: 'axe',
+    weight: 15
+  },
+  {
+    name: 'bat',
+    weight: 13
+  },
+  {
+    name: 'trophy',
+    weight: 25
+  },
+  {
+    name: 'pistol',
+    weight: 20
+  }
 ];
-
 
 // ITERATION 2
 
 function selectRandom(arr) {
-  let randomPick= Math.floor(Math.random() * arr.length);
-  return arr[randomPick]
+  let randomPick = Math.floor(Math.random() * arr.length);
+  return arr[randomPick];
 }
 
 function pickMystery() {
@@ -161,16 +161,15 @@ function pickMystery() {
   let selectWeapon = selectRandom(weaponsArray);
   mystery.suspect = selectSuspect;
   mystery.room = selectRoom;
-  mystery.weapon = selectWeapon
-  return mystery
+  mystery.weapon = selectWeapon;
+  return mystery;
 }
-
 
 // ITERATION 3
 
-function revealMystery() {}
-
-
+function revealMystery(mystery) {
+  return `${mystery.suspect.firstName} ${mystery.suspect.lastName} killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
